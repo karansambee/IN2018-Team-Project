1579e82ed50ea67c08fa0df7157ed9555bfeb55b
Captain ALM <captainalm@captainalm.com>
2023-04-01 23:54:51 +0100

    Update Samina's PersonalInformation class.

--- src/skywaysolutions/app/utils/PersonalInformation.java
+++ src/skywaysolutions/app/utils/PersonalInformation.java
@@ -3,7 +3,7 @@ package skywaysolutions.app.utils;
 import java.util.Date;
 
 /**
- *
+ * This is personalInformation class.
  *
  * @author Samina Matin
  */
@@ -18,17 +18,18 @@ public final class PersonalInformation {
     private String streetName;
 
     /**
+     * This constructs new personalInformation class.
      *
-     *
-     * @param firstName
-     * @param lastName
-     * @param phoneNumber
-     * @param emailAddress
-     * @param dateOfBirth
-     * @param postcode
-     * @param houseNumber
-     * @param streetName
+     * @param firstName This is the first name of the person.
+     * @param lastName This is the last name of the person.
+     * @param phoneNumber This is the phone number of the person.
+     * @param emailAddress This is the email address of the person.
+     * @param dateOfBirth This is the date of birth of the person.
+     * @param postcode This is the postcode of the person.
+     * @param houseNumber This is the house number of the person.
+     * @param streetName This is the street name of the person.
      */
+
     public PersonalInformation(String firstName, String lastName, String phoneNumber, String emailAddress, Date dateOfBirth, String postcode, String houseNumber, String streetName) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -54,62 +55,133 @@ public final class PersonalInformation {
      *
      * @param firstName The new first name.
      */
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * This gets the last name of the person.
+     *
+     * @return The last name.
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * This sets the last name of the person.
+     *
+     * @param lastName The new last name.
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * This gets the phone number of the person.
+     *
+     * @return phoneNumber The phone number.
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * This sets the phone number of the person.
+     *
+     * @param phoneNumber The new phone number.
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * This gets the email address of the person.
+     *
+     * @return emailAddress The email address.
+     */
     public String getEmailAddress() {
         return emailAddress;
     }
 
+    /**
+     * This sets the email address of the person.
+     *
+     * @param emailAddress The new email address.
+     */
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
 
+    /**
+     * This gets the date of birth of the person.
+     *
+     * @return dateOfBirth The date of birth.
+     */
     public Date getDateOfBirth() {
         return dateOfBirth;
     }
 
+    /**
+     * This sets the date of birth of the person.
+     *
+     * @param dateOfBirth The new date of birth.
+     */
     public void setDateOfBirth(Date dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
+    /**
+     * This gets the postcode of the person.
+     *
+     * @return postcode The postcode
+     */
     public String getPostcode() {
         return postcode;
     }
 
+    /**
+     * This sets the postcode of the person.
+     *
+     * @param postcode The new postcode.
+     */
     public void setPostcode(String postcode) {
         this.postcode = postcode;
     }
 
+    /**
+     * This gets the house number of the person.
+     *
+     * @return houseNumber The house number.
+     */
     public String getHouseNumber() {
         return houseNumber;
     }
 
+    /**
+     * This sets the house number of the person.
+     *
+     * @param houseNumber The new house number.
+     */
     public void setHouseNumber(String houseNumber) {
         this.houseNumber = houseNumber;
     }
 
+    /**
+     * This gets the street number of the person.
+     *
+     * @return streetNumber The new street number.
+     */
     public String getStreetName() {
         return streetName;
     }
 
+    /**
+     * This sets the street number of the person.
+     *
+     * @param streetName The new street number.
+     */
     public void setStreetName(String streetName) {
         this.streetName = streetName;
     }
